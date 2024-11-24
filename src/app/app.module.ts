94e2c7754f6b84916f122a60c0e89ceae57223d7
zcarroll4 <zachcarroll2015@gmail.com>
2024-11-24 16:06:38 -0500

    rework project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaxCalculatorComponent } from './tax-calculator/tax-calculator.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaxCalculatorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
